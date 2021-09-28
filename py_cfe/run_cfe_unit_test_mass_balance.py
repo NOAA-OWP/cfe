d4b6883e8a221a29ef17447dc9d0feb315dbc9bf
Jonathan Frame <jmframe@crimson.ua.edu>
2021-09-28 14:31:41 -0600

    INCOMPLETE Python BMI

--- py_cfe/run_cfe_unit_test_mass_balance.py
+++ py_cfe/run_cfe_unit_test_mass_balance.py
@@ -2,6 +2,6 @@ import time
 import numpy as np
 import pandas as pd
 import json
-import cfe
-cfe1 = cfe.CFE('./cat_58_config_cfe.json')
+import cfe_orig
+cfe1 = cfe_orig.CFE('./cat_58_config_cfe.json')
 cfe1.run_unit_test()
