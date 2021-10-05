12cad61049b3829fa556ae34ff0b368ff91fef93
Jonathan Frame <jmframe@crimson.ua.edu>
2021-10-04 21:13:50 -0600

    getter and setter might be working, but need to check carefully

--- py_cfe/bmi_cfe.py
+++ py_cfe/bmi_cfe.py
@@ -18,12 +18,12 @@ class BMI_CFE():
         # these need to be initialized here as scale_output() called in update()
         self.streamflow_cms = 0.0
         self.streamflow_fms = 0.0
-        self.surface_runoff_mm = 0.0
+        self.surface_runoff_m = 0.0
 
         #----------------------------------------------
         # Required, static attributes of the model
         #----------------------------------------------
-        _att_map = {
+        self._att_map = {
             'model_name':         'Conceptual Functional Equivalent (CFE)',
             'version':            '1.0',
             'author_name':        'Jonathan Martin Frame',
@@ -34,14 +34,14 @@ class BMI_CFE():
         #---------------------------------------------
         # Input variable names (CSDMS standard names)
         #---------------------------------------------
-        _input_var_names = [
+        self._input_var_names = [
             'atmosphere_water__time_integral_of_precipitation_mass_flux',
             'water_potential_evaporation_flux']
     
         #---------------------------------------------
         # Output variable names (CSDMS standard names)
         #---------------------------------------------
-        _output_var_names = ['land_surface_water__runoff_depth', 
+        self._output_var_names = ['land_surface_water__runoff_depth', 
                              'land_surface_water__runoff_volume_flux']
         
         #------------------------------------------------------
@@ -50,10 +50,9 @@ class BMI_CFE():
         # This is going to get long, 
         #     since the input variable names could come from any forcing...
         #------------------------------------------------------
-        #_var_name_map_long_first = {
-        _var_name_units_map = {
+        self._var_name_units_map = {
                                 'land_surface_water__runoff_volume_flux':['streamflow_cfs','ft3 s-1'],
-                                'land_surface_water__runoff_depth':['streamflow_mm','mm'],
+                                'land_surface_water__runoff_depth':['total_discharge','m'],
                                 #--------------   Dynamic inputs --------------------------------
                                 'atmosphere_water__time_integral_of_precipitation_mass_flux':['timestep_rainfall_input_m','kg m-2'],
                                 'water_potential_evaporation_flux':['potential_et_m_per_s','m s-1']
@@ -68,7 +67,20 @@ class BMI_CFE():
     # BMI: Model Control Function
     def initialize(self, current_time_step=0):
         self.current_time_step=current_time_step
-       
+
+        # ----- Create some lookup tabels from the long variable names --------#
+        self._var_name_map_long_first = {long_name:self._var_name_units_map[long_name][0] for long_name in self._var_name_units_map.keys()}
+        self._var_name_map_short_first = {self._var_name_units_map[long_name][0]:long_name for long_name in self._var_name_units_map.keys()}
+        self._var_units_map = {long_name:self._var_name_units_map[long_name][1] for long_name in self._var_name_units_map.keys()}
+        
+        # -------------- Initalize all the variables --------------------------# 
+        # -------------- so that they'll be picked up with the get functions --#
+        for var_name in list(self._var_name_units_map.keys()):
+            # ---------- All the variables are single values ------------------#
+            # ---------- so just set to zero for now.        ------------------#
+            self._values[var_name] = 0
+            setattr( self, var_name, 0 )
+
         ############################################################
         # ________________________________________________________ #
         # GET VALUES FROM CONFIGURATION FILE.                      #
@@ -198,6 +210,10 @@ class BMI_CFE():
         # Nash cascade        
         self.K_nash = 0.03
 
+        # ----------- The output is area normalized, this is needed to un-normalize it
+        #                         mm->m                             km2 -> m2          hour->s    
+        self.output_factor_cms =  (1/1000) * (self.catchment_area_km2 * 1000*1000) * (1/3600)
+
         ####################################################################
         # ________________________________________________________________ #
         # ________________________________________________________________ #
@@ -213,6 +229,7 @@ class BMI_CFE():
     # BMI: Model Control Function
     def update(self):
         self.cfe_model.run_cfe(self)
+        self.scale_output()
 
     # __________________________________________________________________________________________________________
     # __________________________________________________________________________________________________________
@@ -220,6 +237,7 @@ class BMI_CFE():
     def update_until(self, until, verbose=True):
         for i in range(self.current_time_step, until):
             self.cfe_model.run_cfe()
+            self.scale_output()
             print("total discharge: {}".format(self.total_discharge))
             print("at time: {}".format(self.current_time))
         
@@ -411,3 +429,331 @@ class BMI_CFE():
             plt.legend()
             plt.show()
             plt.close()
+    
+    
+    #------------------------------------------------------------ 
+    def scale_output(self):
+            
+        self.surface_runoff_m = self.total_discharge
+        self._values['land_surface_water__runoff_depth'] = self.surface_runoff_m/1000
+        self.streamflow_cms = self._values['land_surface_water__runoff_depth'] * self.output_factor_cms
+
+        self._values['land_surface_water__runoff_volume_flux'] = self.streamflow_cms * (1/35.314)
+
+
+    #---------------------------------------------------------------------------- 
+    def initialize_forcings(self):
+        for forcing_name in self.cfg_train['dynamic_inputs']:
+            setattr(self, self._var_name_map_short_first[forcing_name], 0)
+
+    #-------------------------------------------------------------------
+    #-------------------------------------------------------------------
+    # BMI: Model Information Functions
+    #-------------------------------------------------------------------
+    #-------------------------------------------------------------------
+    
+    def get_attribute(self, att_name):
+    
+        try:
+            return self._att_map[ att_name.lower() ]
+        except:
+            print(' ERROR: Could not find attribute: ' + att_name)
+
+    #--------------------------------------------------------
+    # Note: These are currently variables needed from other
+    #       components vs. those read from files or GUI.
+    #--------------------------------------------------------   
+    def get_input_var_names(self):
+
+        return self._input_var_names
+
+    def get_output_var_names(self):
+ 
+        return self._output_var_names
+
+    #------------------------------------------------------------ 
+    def get_component_name(self):
+        """Name of the component."""
+        return self.get_attribute( 'model_name' ) #JG Edit
+
+    #------------------------------------------------------------ 
+    def get_input_item_count(self):
+        """Get names of input variables."""
+        return len(self._input_var_names)
+
+    #------------------------------------------------------------ 
+    def get_output_item_count(self):
+        """Get names of output variables."""
+        return len(self._output_var_names)
+
+    #------------------------------------------------------------ 
+    def get_value(self, var_name):
+        """Copy of values.
+        Parameters
+        ----------
+        var_name : str
+            Name of variable as CSDMS Standard Name.
+        dest : ndarray
+            A numpy array into which to place the values.
+        Returns
+        -------
+        array_like
+            Copy of values.
+        """
+        return self.get_value_ptr(var_name)
+
+    #-------------------------------------------------------------------
+    def get_value_ptr(self, var_name):
+        """Reference to values.
+        Parameters
+        ----------
+        var_name : str
+            Name of variable as CSDMS Standard Name.
+        Returns
+        -------
+        array_like
+            Value array.
+        """
+        return self._values[var_name]
+
+    #-------------------------------------------------------------------
+    #-------------------------------------------------------------------
+    # BMI: Variable Information Functions
+    #-------------------------------------------------------------------
+    #-------------------------------------------------------------------
+    def get_var_name(self, long_var_name):
+                              
+        return self._var_name_map_long_first[ long_var_name ]
+
+    #-------------------------------------------------------------------
+    def get_var_units(self, long_var_name):
+
+        return self._var_units_map[ long_var_name ]
+                                                             
+    #-------------------------------------------------------------------
+    def get_var_type(self, long_var_name):
+        """Data type of variable.
+
+        Parameters
+        ----------
+        var_name : str
+            Name of variable as CSDMS Standard Name.
+
+        Returns
+        -------
+        str
+            Data type.
+        """
+        # JG Edit
+        return self.get_value_ptr(long_var_name)  #.dtype
+    
+    #------------------------------------------------------------ 
+    def get_var_grid(self, name):
+        
+        # JG Edit
+        # all vars have grid 0 but check if its in names list first
+        if name in (self._output_var_names + self._input_var_names):
+            return self._var_grid_id  
+
+    #------------------------------------------------------------ 
+    def get_var_itemsize(self, name):
+#        return np.dtype(self.get_var_type(name)).itemsize
+        return np.array(self.get_value(name)).itemsize
+
+    #------------------------------------------------------------ 
+    def get_var_location(self, name):
+        
+        # JG Edit
+        # all vars have location node but check if its in names list first
+        if name in (self._output_var_names + self._input_var_names):
+            return self._var_loc
+
+    #-------------------------------------------------------------------
+    # JG Note: what is this used for?
+    def get_var_rank(self, long_var_name):
+
+        return np.int16(0)
+
+    #-------------------------------------------------------------------
+    def get_start_time( self ):
+    
+        return self._start_time #JG Edit
+
+    #-------------------------------------------------------------------
+    def get_end_time( self ):
+
+        return self._end_time #JG Edit
+
+
+    #-------------------------------------------------------------------
+    def get_current_time( self ):
+
+        return self.t #JG Edit
+
+    #-------------------------------------------------------------------
+    def get_time_step( self ):
+
+        return self.get_attribute( 'time_step_size' ) #JG: Edit
+
+    #-------------------------------------------------------------------
+    def get_time_units( self ):
+
+        return self.get_attribute( 'time_units' ) 
+       
+    #-------------------------------------------------------------------
+    def set_value(self, var_name, value):
+        """Set model values.
+
+        Parameters
+        ----------
+        var_name : str
+            Name of variable as CSDMS Standard Name.
+        src : array_like
+              Array of new values.
+        """ 
+        setattr( self, self.get_var_name(var_name), value )
+        self._values[var_name] = value
+
+    #------------------------------------------------------------ 
+    def set_value_at_indices(self, name, inds, src):
+        """Set model values at particular indices.
+        Parameters
+        ----------
+        var_name : str
+            Name of variable as CSDMS Standard Name.
+        src : array_like
+            Array of new values.
+        indices : array_like
+            Array of indices.
+        """
+        # JG Note: TODO confirm this is correct. Get/set values ~=
+#        val = self.get_value_ptr(name)
+#        val.flat[inds] = src
+
+        #JMFrame: chances are that the index will be zero, so let's include that logic
+        if np.array(self.get_value(name)).flatten().shape[0] == 1:
+            self.set_value(name, src)
+        else:
+            # JMFrame: Need to set the value with the updated array with new index value
+            val = self.get_value_ptr(name)
+            for i in inds.shape:
+                val.flatten()[inds[i]] = src[i]
+            self.set_value(name, val)
+
+    #------------------------------------------------------------ 
+    def get_var_nbytes(self, var_name):
+        """Get units of variable.
+        Parameters
+        ----------
+        var_name : str
+            Name of variable as CSDMS Standard Name.
+        Returns
+        -------
+        int
+            Size of data array in bytes.
+        """
+        # JMFrame NOTE: Had to import sys for this function
+        return sys.getsizeof(self.get_value_ptr(var_name))
+
+    #------------------------------------------------------------ 
+    def get_value_at_indices(self, var_name, dest, indices):
+        """Get values at particular indices.
+        Parameters
+        ----------
+        var_name : str
+            Name of variable as CSDMS Standard Name.
+        dest : ndarray
+            A numpy array into which to place the values.
+        indices : array_like
+            Array of indices.
+        Returns
+        -------
+        array_like
+            Values at indices.
+        """
+        #JMFrame: chances are that the index will be zero, so let's include that logic
+        if np.array(self.get_value(var_name)).flatten().shape[0] == 1:
+            return self.get_value(var_name)
+        else:
+            val_array = self.get_value(var_name).flatten()
+            return np.array([val_array[i] for i in indices])
+
+    # JG Note: remaining grid funcs do not apply for type 'scalar'
+    #   Yet all functions in the BMI must be implemented 
+    #   See https://bmi.readthedocs.io/en/latest/bmi.best_practices.html          
+    #------------------------------------------------------------ 
+    def get_grid_edge_count(self, grid):
+        raise NotImplementedError("get_grid_edge_count")
+
+    #------------------------------------------------------------ 
+    def get_grid_edge_nodes(self, grid, edge_nodes):
+        raise NotImplementedError("get_grid_edge_nodes")
+
+    #------------------------------------------------------------ 
+    def get_grid_face_count(self, grid):
+        raise NotImplementedError("get_grid_face_count")
+    
+    #------------------------------------------------------------ 
+    def get_grid_face_edges(self, grid, face_edges):
+        raise NotImplementedError("get_grid_face_edges")
+
+    #------------------------------------------------------------ 
+    def get_grid_face_nodes(self, grid, face_nodes):
+        raise NotImplementedError("get_grid_face_nodes")
+    
+    #------------------------------------------------------------ 
+    def get_grid_node_count(self, grid):
+        raise NotImplementedError("get_grid_node_count")
+
+    #------------------------------------------------------------ 
+    def get_grid_nodes_per_face(self, grid, nodes_per_face):
+        raise NotImplementedError("get_grid_nodes_per_face") 
+    
+    #------------------------------------------------------------ 
+    def get_grid_origin(self, grid_id, origin):
+        raise NotImplementedError("get_grid_origin") 
+
+    #------------------------------------------------------------ 
+    def get_grid_rank(self, grid_id):
+ 
+        # JG Edit
+        # 0 is the only id we have
+        if grid_id == 0: 
+            return 1
+
+    #------------------------------------------------------------ 
+    def get_grid_shape(self, grid_id, shape):
+        raise NotImplementedError("get_grid_shape") 
+
+    #------------------------------------------------------------ 
+    def get_grid_size(self, grid_id):
+       
+        # JG Edit
+        # 0 is the only id we have
+        if grid_id == 0:
+            return 1
+
+    #------------------------------------------------------------ 
+    def get_grid_spacing(self, grid_id, spacing):
+        raise NotImplementedError("get_grid_spacing") 
+
+    #------------------------------------------------------------ 
+    def get_grid_type(self, grid_id=0):
+
+        # JG Edit
+        # 0 is the only id we have        
+        if grid_id == 0:
+            return 'scalar'
+
+    #------------------------------------------------------------ 
+    def get_grid_x(self):
+        raise NotImplementedError("get_grid_x") 
+
+    #------------------------------------------------------------ 
+    def get_grid_y(self):
+        raise NotImplementedError("get_grid_y") 
+
+    #------------------------------------------------------------ 
+    def get_grid_z(self):
+        raise NotImplementedError("get_grid_z") 
+
