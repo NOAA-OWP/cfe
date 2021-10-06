6eecf0bbdf052062cc4bc078a5c5668d85a91c5c
Scott D. Peckham <Scott.Peckham@colorado.edu>
2021-10-06 16:46:41 -0600

    Fixed several compiler warnings

--- forcing_code/src/pet.c
+++ forcing_code/src/pet.c
@@ -425,7 +425,7 @@ void itwo_alloc_pet(int ***array, int rows, int cols) {
 
     if ((rows == 0) || (cols == 0)) {
         printf("Error: Attempting to allocate array of size 0\n");
-        exit;
+        exit(-9);
     }
 
     frows = rows + 1;  /* added one for FORTRAN numbering */
@@ -454,7 +454,7 @@ void dtwo_alloc_pet(double ***array, int rows, int cols) {
 
     if ((rows == 0) || (cols == 0)) {
         printf("Error: Attempting to allocate array of size 0\n");
-        exit;
+        exit(-9);
     }
 
     frows = rows + 1;  /* added one for FORTRAN numbering */
