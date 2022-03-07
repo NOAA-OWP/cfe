5f108c1cacf168fd5a1dbf0ed0e636ffd9da4f43
Ahmad Jan <ajkhattak@gmail.com>
2022-03-07 17:43:27 -0500

    Reverting to 180c56f to use it in the pseudo framework.

--- src/bmi_cfe.c
+++ src/bmi_cfe.c
@@ -11,8 +11,8 @@
 
 #define CFE_DEGUG 0
 
-#define INPUT_VAR_NAME_COUNT 2
-#define OUTPUT_VAR_NAME_COUNT 6
+#define INPUT_VAR_NAME_COUNT 4
+#define OUTPUT_VAR_NAME_COUNT 9
 #define STATE_VAR_NAME_COUNT 90   // must match var_info array size
 
 #define PARAM_VAR_NAME_COUNT 10
@@ -168,7 +168,10 @@ static const char *output_var_names[OUTPUT_VAR_NAME_COUNT] = {
         "GIUH_RUNOFF",
         "NASH_LATERAL_RUNOFF",
         "DEEP_GW_TO_CHANNEL_FLUX",
-        "Q_OUT"
+        "Q_OUT",
+	"SMCT",
+	"SMCT_CHANGE",
+	"SURF_RUNOFF_SCHEME",
 };
 
 static const char *output_var_types[OUTPUT_VAR_NAME_COUNT] = {
@@ -177,7 +180,10 @@ static const char *output_var_types[OUTPUT_VAR_NAME_COUNT] = {
         "double",
         "double",
         "double",
-        "double"
+        "double",
+	"double",
+	"double",
+	"int"
 };
 
 static const int output_var_item_count[OUTPUT_VAR_NAME_COUNT] = {
@@ -186,7 +192,10 @@ static const int output_var_item_count[OUTPUT_VAR_NAME_COUNT] = {
         1,
         1,
         1,
-        1
+        1,
+	1,
+	1,
+	1
 };
 
 static const char *output_var_units[OUTPUT_VAR_NAME_COUNT] = {
@@ -195,7 +204,10 @@ static const char *output_var_units[OUTPUT_VAR_NAME_COUNT] = {
         "m",
         "m",
         "m",
-        "m"
+        "m",
+	"m",
+	"m",
+	""
 };
 
 static const int output_var_grids[OUTPUT_VAR_NAME_COUNT] = {
@@ -204,7 +216,10 @@ static const int output_var_grids[OUTPUT_VAR_NAME_COUNT] = {
         0,
         0,
         0,
-        0
+        0,
+	0,
+	0,
+	0
 };
 
 static const char *output_var_locations[OUTPUT_VAR_NAME_COUNT] = {
@@ -213,38 +228,53 @@ static const char *output_var_locations[OUTPUT_VAR_NAME_COUNT] = {
         "node",
         "node",
         "node",
-        "node"
+        "node",
+	"node",
+	"node",
+	" "
 };
 
 // Don't forget to update Get_value/Get_value_at_indices (and setter) implementation if these are adjusted
 static const char *input_var_names[INPUT_VAR_NAME_COUNT] = {
         "atmosphere_water__liquid_equivalent_precipitation_rate",
-        "water_potential_evaporation_flux"
+        "water_potential_evaporation_flux",
+	"soil__ice_fraction_schaake",
+	"soil__ice_fraction_xinan"
 };
 
 static const char *input_var_types[INPUT_VAR_NAME_COUNT] = {
         "double",
-        "double"
+        "double",
+	"double",
+	"double"
 };
 
 static const char *input_var_units[INPUT_VAR_NAME_COUNT] = {
         "mm h-1", //"atmosphere_water__liquid_equivalent_precipitation_rate"
         "m s-1",   //"water_potential_evaporation_flux"
+	"m",    // ice fraction in meters
+	"none",     // ice fraction [-]
 };
 
 static const int input_var_item_count[INPUT_VAR_NAME_COUNT] = {
         1,
-        1
+        1,
+	1,
+	1
 };
 
 static const char input_var_grids[INPUT_VAR_NAME_COUNT] = {
         0,
-        0
+        0,
+	0,
+	0
 };
 
 static const char *input_var_locations[INPUT_VAR_NAME_COUNT] = {
         "node",
-        "node"
+        "node",
+	"node",
+	"node"
 };
 
 static int Get_start_time (Bmi *self, double * time)
