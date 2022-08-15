f6c2335cdbe9fdf65c9fe51a890420572cb593bf
Ahmad Jan <ajkhattak@gmail.com>
2022-08-15 15:03:00 -0400

    added bash script to run cfe with different options. documenation needs to be updated.

---
+++ run_cfe.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+flag=$1
+
+echo "$#"
+if [ ! $# == 1 ]; then
+    echo "Usage: $0 OPTION={BASE,FORCING,FORCINGPET,AETROOTZONE}"
+    echo "One of these options must be specified to run CFE"
+  exit
+fi
+echo "$flag"
+args=" "
+exe_name=" "
+if [ $flag == "BASE" ]; then
+    args='./configs/cat_87_bmi_config_cfe.txt'
+    exe_name='cfe_base'
+else if [ $flag == "FORCING" ]; then
+	 args="./configs/cat_87_bmi_config_cfe_pass.txt ./configs/cat_87_bmi_config_aorc.txt"
+	 exe_name='cfe_forcing'
+     else if [ $flag == "FORCINGPET" ]; then
+	      args='./configs/cat_87_bmi_config_cfe_pass.txt ./configs/cat_87_bmi_config_aorc.txt ./configs/cat_87_bmi_config_pet_pass.txt'
+	      exe_name='cfe_forcingpet'
+	  else if [ $flag == "AETROOTZONE" ]; then
+		   args='./configs/laramie_bmi_config_cfe_pass.txt ./configs/laramie_bmi_config_aorc.txt ./configs/laramie_bmi_config_pet_pass.txt ./configs/laramie_bmi_config_smc_coupler.txt'
+		   exe_name='cfe_aet_rootzone'
+	       fi
+	  fi
+     fi
+fi
+echo "config file: $args"
+./build/${exe_name} $args
