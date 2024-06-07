8c212826a65c45fd0e11770cb3a5729ada3311a9
Ahmad Jan <ajkhattak@gmail.com>
2024-06-07 13:07:03 -0400

    config files cleanup, moved aorc and pet files to their respective repos.

--- run_cfe.sh
+++ run_cfe.sh
@@ -8,27 +8,26 @@ if [ ! $# == 1 ]; then
 fi
 
 if [ $flag == "BASE" ] || [ "$flag" == "FORCING" ] || [ "$flag" == "FORCINGPET" ] || [ "$flag" == "AETROOTZONE" ]; then
-    #or ($flag != "FORCING") or ($flag != "FORCINGPET") or ($flag != "AETROOTZONE") ]; then
-echo "CFE running with option $flag"
+    echo "CFE running with option $flag"
 else
-echo "Invalid option! $flag"
-exit
+    echo "Invalid option! $flag"
+    exit
 fi
 
 
 args=" "
 exe_name=" "
 if [ $flag == "BASE" ]; then
-    args='./configs/cat_87_bmi_config_cfe.txt'
+    args='./configs/cfe_config_cat_87.txt'
     exe_name='cfe_base'
 else if [ $flag == "FORCING" ]; then
-	 args="./configs/cat_87_bmi_config_cfe_pass.txt ./configs/cat_87_bmi_config_aorc.txt"
+	 args="./configs/cfe_config_cat_87_pass.txt ./extern/aorc_bmi/configs/aorc_config_cat_87.txt"
 	 exe_name='cfe_forcing'
      else if [ $flag == "FORCINGPET" ]; then
-	      args='./configs/cat_87_bmi_config_cfe_pass.txt ./configs/cat_87_bmi_config_aorc.txt ./configs/cat_87_bmi_config_pet_pass.txt'
+	      args='./configs/cfe_config_cat_87_pass.txt ./extern/aorc_bmi/configs/aorc_config_cat_87.txt ./extern/evapotranspiration/configs/pet_config_cat_87_pass.txt'
 	      exe_name='cfe_forcingpet'
 	  else if [ $flag == "AETROOTZONE" ]; then
-		   args='./configs/laramie_bmi_config_cfe_pass_aet_rz.txt ./configs/laramie_bmi_config_aorc.txt ./configs/laramie_bmi_config_pet_pass.txt ./configs/laramie_bmi_config_smc_coupler.txt'
+		   args='./configs/cfe_config_laramie_pass_aet_rz.txt ./extern/aorc_bmi/configs/aorc_config_laramie.txt ./extern/evapotranspiration/configs/pet_config_laramie_pass.txt ./configs/smp_config_laramie.txt'
 		   exe_name='cfe_aet_rootzone'
 	       fi
 	  fi
