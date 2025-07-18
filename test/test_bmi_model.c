d05b22f0deed9b1bea4a74fd452765792cacb17d
Robert Bartel <robert.bartel@noaa.gov>
2025-07-18 09:16:59 -0500

    Add more BMI unit tests.
    Adding for remaining grid functions and for time functions.

--- test/test_bmi_model.c
+++ test/test_bmi_model.c
@@ -1,7 +1,7 @@
 #include "general_test_utils.h"
 #include "bmi_test_utils.h"
 #include "bmi_cfe.h"
-
+#include "cfe.h"
 
 int test_get_component_name(TestFixture* fixture)
 {
@@ -14,6 +14,104 @@ int test_get_component_name(TestFixture* fixture)
     if (confirm_matches_expected_strs(EXPECTED_COMPONENT_NAME, name))
         return TEST_RETURN_CODE_PASS;
     return TEST_RETURN_CODE_FAIL;
+}
+
+int test_get_current_time(TestFixture* fixture)
+{
+    int bmi_status;
+    double current_time;
+
+    // For this, we need to be able to initialize first
+    bmi_status = fixture->bmi_model->initialize(fixture->bmi_model, fixture->cfg_file);
+    if (bmi_status != BMI_SUCCESS) {
+        printf("\nReturned BMI_FAILURE status code attempting to initialize (in order to test getting current time)");
+        return TEST_RETURN_CODE_FAIL;
+    }
+
+    // Start by checking the initial value, which should be the same as the start time (0.0)
+    bmi_status = fixture->bmi_model->get_current_time(fixture->bmi_model, &current_time);
+    if (bmi_status == BMI_FAILURE) {
+        printf("\nReturned BMI_FAILURE status code attempting to get initial current time");
+        return TEST_RETURN_CODE_FAIL;
+    }
+    if (!confirm_matches_expected_doubles(0.0, current_time)) {
+        printf("\nDid not match initial expected module current time");
+        return TEST_RETURN_CODE_FAIL;
+    }
+
+    // Next, update the model one time step (failing if we couldn't set the variables properly)
+    if (!set_module_input_variables_before_update(fixture, 1, current_time)) {
+        printf("\nCouldn't set module input variables before running first update (while testing getting current time");
+        return TEST_RETURN_CODE_FAIL;
+    }
+    bmi_status = fixture->bmi_model->update(fixture->bmi_model);
+    if (bmi_status == BMI_FAILURE) {
+        printf("\nReturned BMI_FAILURE status code attempting to advance module with 'update' to test current time");
+        return TEST_RETURN_CODE_FAIL;
+    }
+
+    // Now make sure the current time value updates as expected
+    bmi_status = fixture->bmi_model->get_current_time(fixture->bmi_model, &current_time);
+    if (bmi_status == BMI_FAILURE) {
+        printf("\nReturned BMI_FAILURE status code attempting to get current time after advancing 1 time step");
+        return TEST_RETURN_CODE_FAIL;
+    }
+    if (!confirm_matches_expected_doubles(3600.0, current_time)) {
+        printf("\nDid not match expected module current time after advancing 1 time step");
+        return TEST_RETURN_CODE_FAIL;
+    }
+
+    return TEST_RETURN_CODE_PASS;
+}
+
+int test_get_end_time(TestFixture* fixture)
+{
+
+    // The tests use the standard boolean macros from <stdbool.h>, but CFE also defines and uses its own
+    // custom TRUE and FALSE macros, in particular in get_end_time.  So, sanity check them to be safe
+#ifdef TRUE
+    if (TRUE != true) {
+        printf("\nConflicting definitions for stdbool.h 'true' (%i) and CFE internal 'TRUE' (%i) macros", true, TRUE);
+        printf("\nThis may have implications beyond this test, but is problematic in particular for get_end_time");
+        return TEST_RETURN_CODE_FAIL;
+    }
+#endif
+#ifdef FALSE
+    if (FALSE != false) {
+        printf("\nConflicting definitions for stdbool.h 'false' (%i) and CFE internal 'FALSE' (%i) macros", false, FALSE);
+        printf("\nThis may have implications beyond this test, but is problematic in particular for get_end_time");
+        return TEST_RETURN_CODE_FAIL;
+    }
+#endif
+
+    int bmi_status;
+    double end_time;
+
+    // For this, we need to be able to initialize first
+    bmi_status = fixture->bmi_model->initialize(fixture->bmi_model, fixture->cfg_file);
+    if (bmi_status != BMI_SUCCESS) {
+        printf("\nReturned BMI_FAILURE status code attempting to initialize (in order to test getting end time)");
+        return TEST_RETURN_CODE_FAIL;
+    }
+
+    bmi_status = fixture->bmi_model->get_end_time(fixture->bmi_model, &end_time);
+    if (bmi_status == BMI_FAILURE) {
+        printf("\nReturned BMI_FAILURE status code attempting to get end time");
+        return TEST_RETURN_CODE_FAIL;
+    }
+
+    // The testing for this is odd ... for now, fail with an error in this scenario, and note test needs extension
+    if (((cfe_state_struct*)(fixture->bmi_model->data))->is_forcing_from_bmi != true) {
+        printf("\nTesting of get_end_time not implemented for if module directly reads its own forcings");
+        return TEST_RETURN_CODE_FAIL;
+    }
+
+    // Assume expected end time is FLT_MAX
+    if (!confirm_matches_expected_doubles(FLT_MAX, end_time)) {
+        printf("\nDid not match expected module end time");
+        return TEST_RETURN_CODE_FAIL;
+    }
+    return TEST_RETURN_CODE_PASS;
 }
 
 int test_get_grid_rank(TestFixture* fixture)
@@ -30,6 +128,48 @@ int test_get_grid_rank(TestFixture* fixture)
         // For now, all grids are of rank 1
         if (!confirm_matches_expected_ints(1, actual_rank)) {
             printf("\nGrid rank for '%s' did not match expected", fixture->expected_output_and_input_var_names[i]);
+            return TEST_RETURN_CODE_FAIL;
+        }
+    }
+    return TEST_RETURN_CODE_PASS;
+}
+
+int test_get_grid_size(TestFixture* fixture)
+{
+    int status, actual_size;
+
+    for (int i = 0; i < EXPECTED_TOTAL_VAR_COUNT; i++) {
+        status = fixture->bmi_model->get_grid_rank(fixture->bmi_model, fixture->expected_grid_ids[i], &actual_size);
+        if (status != BMI_SUCCESS) {
+            printf("\nReturned BMI_FAILURE status code getting grid size for '%s'", fixture->expected_output_and_input_var_names[i]);
+            return TEST_RETURN_CODE_FAIL;
+        }
+
+        // For now, all grids are of size 1
+        if (!confirm_matches_expected_ints(1, actual_size)) {
+            printf("\nGrid size for '%s' did not match expected", fixture->expected_output_and_input_var_names[i]);
+            return TEST_RETURN_CODE_FAIL;
+        }
+    }
+    return TEST_RETURN_CODE_PASS;
+}
+
+int test_get_grid_type(TestFixture* fixture)
+{
+    int status;
+
+    char actual_type[BMI_MAX_COMPONENT_NAME];
+
+    for (int i = 0; i < EXPECTED_TOTAL_VAR_COUNT; i++) {
+        status = fixture->bmi_model->get_grid_type(fixture->bmi_model, fixture->expected_grid_ids[i], actual_type);
+        if (status != BMI_SUCCESS) {
+            printf("\nReturned BMI_FAILURE status code getting grid type for '%s'", fixture->expected_output_and_input_var_names[i]);
+            return TEST_RETURN_CODE_FAIL;
+        }
+
+        // For now, all grids are "scalar"
+        if (!confirm_matches_expected_strs("scalar", actual_type)) {
+            printf("\nGrid type for '%s' did not match expected", fixture->expected_output_and_input_var_names[i]);
             return TEST_RETURN_CODE_FAIL;
         }
     }
@@ -114,6 +254,57 @@ int test_get_output_var_names(TestFixture* fixture)
     return result;
 }
 
+int test_get_start_time(TestFixture* fixture)
+{
+    int bmi_status;
+    double start_time;
+    bmi_status = fixture->bmi_model->get_start_time(fixture->bmi_model, &start_time);
+    if (bmi_status == BMI_FAILURE) {
+        printf("\nReturned BMI_FAILURE status code attempting to get start time");
+        return TEST_RETURN_CODE_FAIL;
+    }
+    // Assume expected start time is 0.0
+    if (!confirm_matches_expected_doubles(0.0, start_time)) {
+        printf("\nDid not match expected module start time");
+        return TEST_RETURN_CODE_FAIL;
+    }
+    return TEST_RETURN_CODE_PASS;
+}
+
+int test_get_time_step(TestFixture* fixture)
+{
+    int bmi_status;
+    double time_step;
+    bmi_status = fixture->bmi_model->get_time_step(fixture->bmi_model, &time_step);
+    if (bmi_status == BMI_FAILURE) {
+        printf("\nReturned BMI_FAILURE status code attempting to get time step");
+        return TEST_RETURN_CODE_FAIL;
+    }
+    // Assume expected time step is 1 hour, in seconds
+    if (!confirm_matches_expected_doubles(3600.0, time_step)) {
+        printf("\nDid not match expected module time step");
+        return TEST_RETURN_CODE_FAIL;
+    }
+    return TEST_RETURN_CODE_PASS;
+}
+
+int test_get_time_units(TestFixture* fixture)
+{
+    int bmi_status;
+    char time_units[BMI_MAX_UNITS_NAME];
+    bmi_status = fixture->bmi_model->get_time_units(fixture->bmi_model, time_units);
+    if (bmi_status == BMI_FAILURE) {
+        printf("\nReturned BMI_FAILURE status code attempting to get time units");
+        return TEST_RETURN_CODE_FAIL;
+    }
+    // Assume expected time units of seconds (s)
+    if (!confirm_matches_expected_strs("s", time_units)) {
+        printf("\nDid not match expected module time step");
+        return TEST_RETURN_CODE_FAIL;
+    }
+    return TEST_RETURN_CODE_PASS;
+}
+
 int test_get_var_grid(TestFixture* fixture)
 {
     int grid_value, status;
@@ -180,7 +371,6 @@ int test_get_var_location(TestFixture* fixture)
     }
     return TEST_RETURN_CODE_PASS;
 }
-
 
 int test_get_var_units(TestFixture* fixture)
 {
@@ -309,8 +499,16 @@ int main(int argc, const char* argv[])
     // TODO: finalize test
     else if (strcmp(argv[1], "test_get_component_name") == 0)
         result = test_get_component_name(fixture);
+    else if (strcmp(argv[1], "test_get_current_time") == 0)
+        result = test_get_current_time(fixture);
+    else if (strcmp(argv[1], "test_get_end_time") == 0)
+        result = test_get_end_time(fixture);
     else if (strcmp(argv[1], "test_get_grid_rank") == 0)
         result = test_get_grid_rank(fixture);
+    else if (strcmp(argv[1], "test_get_grid_size") == 0)
+        result = test_get_grid_size(fixture);
+    else if (strcmp(argv[1], "test_get_grid_type") == 0)
+        result = test_get_grid_type(fixture);
     else if (strcmp(argv[1], "test_get_input_item_count") == 0)
         result = test_get_input_item_count(fixture);
     else if (strcmp(argv[1], "test_get_input_var_names") == 0)
@@ -319,6 +517,12 @@ int main(int argc, const char* argv[])
         result = test_get_output_item_count(fixture);
     else if (strcmp(argv[1], "test_get_output_var_names") == 0)
         result = test_get_output_var_names(fixture);
+    else if (strcmp(argv[1], "test_get_start_time") == 0)
+        result = test_get_start_time(fixture);
+    else if (strcmp(argv[1], "test_get_time_step") == 0)
+        result = test_get_time_step(fixture);
+    else if (strcmp(argv[1], "test_get_time_units") == 0)
+        result = test_get_time_units(fixture);
     else if (strcmp(argv[1], "test_get_var_grid") == 0)
         result = test_get_var_grid(fixture);
     else if (strcmp(argv[1], "test_get_var_itemsize") == 0)
